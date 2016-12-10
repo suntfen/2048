b4939c9ac6eb6344a8d27b89acf115d734c38076
suntfen <suntfen@163.com>
2016-12-10 12:11:07 +0800

    Update html_actuator.js

--- zhong/js/html_actuator.js
+++ zhong/js/html_actuator.js
@@ -26,7 +26,7 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
 
     if (metadata.terminated) {
       if (metadata.over) {
-        self.message(false,grid.getMaxScore()); // You lose
+        self.message(false); // You lose
       } else if (metadata.won) {
         self.message(true); // You win!
       }
